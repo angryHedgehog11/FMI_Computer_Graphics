5920e177006a5e875b3845c07910084656496765
Radostina Hadzhieva <80592563+angryHedgehog11@users.noreply.github.com>
2023-12-26 14:02:19 +0200

    Create SidePoints.m

---
+++ HW1/SidePoints.m
@@ -0,0 +1,32 @@
+if color~=M(rx+1,ry)          
+      M(rx+1,ry)=color2; 
+end
+ 
+if color~=M(rx,ry+1)          
+      M(rx,ry+1)=color2;
+end
+ 
+if color~=M(rx+1,ry+1)          
+     M(rx+1,ry+1)=color2; 
+end
+   
+if color~=M(rx-1,ry)          
+      M(rx-1,ry)=color2; 
+end
+   
+if color~=M(rx,ry-1)          
+      M(rx,ry-1)=color2; 
+end
+   
+if color~=M(rx-1,ry-1)           
+     M(rx-1,ry-1)=color2; 
+end
+ 
+if color~=M(rx-1,ry+1)          
+      M(rx-1,ry+1)=color2; 
+end
+   
+if color~=M(rx+1,ry-1)           
+      M(rx+1,ry-1)=color2;
+end
+   
